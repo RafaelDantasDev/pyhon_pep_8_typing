c042a9ebde01e66b74473792b8ba1535c9da59d2
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-12 16:36:16 -0300

    Primeira fase do projeto

---
+++ main.py
@@ -0,0 +1,18 @@
+# Instalei a biblioteca mypy no terminal
+
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+
+from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
+
+
+
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatisticas(EstatisticaDetalhada('20/03/2025', 120)))
